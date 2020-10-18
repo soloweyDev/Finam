b4e37d0ae78129ec391f1d39515f6f3776e4bba6
Ануфриев Дмитрий <solowey@narod.ru>
2020-10-18 22:12:19 +0300

    Сделал вывод двнных из бд в твблицу

--- Finam/MainWindow.xaml.cs
+++ Finam/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows;
 
 namespace Finam
 {
@@ -7,9 +11,47 @@ namespace Finam
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly SqlDataAdapter adapter;
+        readonly DataTable chartTable;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string sql = "SELECT * FROM Chart";
+            chartTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection("Data Source=DESKTOP-MY\\SQLEXPRESS;Initial Catalog=Finam;Integrated Security=True");
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                // установка команды на добавление для вызова хранимой процедуры
+                adapter.InsertCommand = new SqlCommand("spGetChart", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                adapter.Fill(chartTable);
+                dg.ItemsSource = chartTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        struct ChartStruct
+        {
+            decimal Open;
+            decimal High;
+            decimal Low;
+            decimal Close;
         }
     }
 }
